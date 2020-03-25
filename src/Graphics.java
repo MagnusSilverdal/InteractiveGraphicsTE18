74c20a3baa2d978b9aecfed79606d80b3a99911b
MagnusSilverdal <magnus.silverdal@ga.ntig.se>
2020-03-25 12:18:14 +0100

    Added Main-class for starting the program and a Graphics-class to handle graphics, timing and actions.

---
+++ src/Graphics.java
@@ -0,0 +1,9 @@
+/**
+ * This is a class
+ * Created 2020-03-25
+ *
+ * @author Magnus Silverdal
+ */
+public class Graphics {
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * This is a class
+ * Created 2020-03-25
+ *
+ * @author Magnus Silverdal
+ */
+public class Main {
+    public static void main(String[] args) {
+        Graphics graphics = new Graphics();
+    }
+}
