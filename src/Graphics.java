890a5241777e235f4ab14a7ce402cef9c5f0ba3e
MagnusSilverdal <magnus.silverdal@ga.ntig.se>
2020-03-26 09:38:33 +0100

    Set up update and render together with timers for both

--- src/Graphics.java
+++ src/Graphics.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 
@@ -9,14 +10,19 @@ import java.awt.image.DataBufferInt;
  *
  * @author Magnus Silverdal
  */
-public class Graphics extends Canvas {
+public class Graphics extends Canvas implements Runnable {
     private String title = "Graphics";
     private int width;
     private int height;
-    
+
     private JFrame frame;
     private BufferedImage image;
     private int[] pixels;
+
+    private Thread thread;
+    private boolean running = false;
+    int fps = 60;
+    int ups = 10;
 
     public Graphics(int w, int h) {
         image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
@@ -30,6 +36,67 @@ public class Graphics extends Canvas {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    private void draw() {
+        BufferStrategy bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+
+        java.awt.Graphics g = bs.getDrawGraphics();
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+        g.dispose();
+        bs.show();
+    }
+
+    private void update() {
+        for (int i = 0 ; i < pixels.length ; i++) {
+            pixels[i] = 0;
+        }
+    }
+
+    public synchronized void start() {
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        double frameUpdateinteval = 1000000000.0 / fps;
+        double stateUpdateinteval = 1000000000.0 / ups;
+        double deltaFrame = 0;
+        double deltaUpdate = 0;
+        long lastTime = System.nanoTime();
+
+        while (running) {
+            long now = System.nanoTime();
+            deltaFrame += (now - lastTime) / frameUpdateinteval;
+            deltaUpdate += (now - lastTime) / stateUpdateinteval;
+            lastTime = now;
+
+            while (deltaUpdate >= 1) {
+                update();
+                deltaUpdate--;
+            }
+
+            while (deltaFrame >= 1) {
+                draw();
+                deltaFrame--;
+            }
+        }
+        stop();
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@
 public class Main {
     public static void main(String[] args) {
         Graphics graphics = new Graphics(1024,800);
+        graphics.start();
     }
 }
